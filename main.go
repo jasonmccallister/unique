7c9a42d7bd91ccda47e9cd5338c33264d6fabd27
Jason McCallister <themccallister@gmail.com>
2019-09-21 18:57:43 -0400

    remove flag from first arg

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	unique "github.com/jasonmccallister/unique/pkg"
 )
@@ -16,19 +17,17 @@ var (
 )
 
 func main() {
-	flag.StringVar(&fileArg, "file", "", "Name of the file to process (relative to the current directory")
+	if len(os.Args) != 2 {
+		fmt.Printf("usage: %s <filename>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
 	flag.IntVar(&columnArg, "column", 0, "Which column should be used for unique values (defaults to the first column which is 0)")
 	flag.BoolVar(&hasHeaderArg, "header", true, "If the file has a header, setting this will bypass the first row of the CSV")
 
 	flag.Parse()
 
-	if fileArg == "" {
-		fmt.Println("\nError: You must pass a file to process")
-		fmt.Println("")
-		flag.PrintDefaults()
-		fmt.Println("")
-		os.Exit(1)
-	}
+	fileArg := os.Args[1]
 
 	file, err := os.Open(fileArg)
 	if err != nil {
@@ -45,5 +44,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(fmt.Sprintf("there are a total of %v unique values for column %v", count, columnArg))
+	fmt.Println(fmt.Sprintf("There are a total of %v unique values for column %v", count, columnArg))
 }
